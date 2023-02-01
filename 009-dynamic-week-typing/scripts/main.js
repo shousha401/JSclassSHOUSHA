26862b591f480ba049dee4399413ffa606a76ee0
shousha401 <shousha401@hotmail.com>
2023-01-31 21:54:33 -0800

    rest of week4 hw

---
+++ 009-dynamic-week-typing/scripts/main.js
@@ -0,0 +1,15 @@
+let foo = 42 // foo is now a number
+console.log("foo is 42 - ",foo)
+
+foo = "bar" // foo is now a string
+console.log("foo is bar - ",foo)
+
+foo = true // foo is now a boolean
+console.log("foo is true - ",foo)
+
+
+ foo = 42; // foo is a number
+const result = foo + "1"; // JavaScript coerces foo to a string, so it can be concatenated with the other operand
+console.log("result is 42 + 1 ...", result) // 421
+
+
---
+++ 010-typeof/scripts/main.js
@@ -0,0 +1,15 @@
+let foo = 42 // foo is now a number
+console.log("foo is 42 - which is typeof ", (typeof foo))
+
+foo = "bar" // foo is now a string
+console.log("foo is bar - which is typeof ", (typeof foo))
+
+foo = true // foo is now a boolean
+console.log("foo is true - which is typeof  ", (typeof foo))
+
+
+ foo = 42; // foo is a number
+const result = foo + "1"; // JavaScript coerces foo to a string, so it can be concatenated with the other operand
+console.log("result is 42 + 1 is typeof - ", (typeof result))// 421
+
+
