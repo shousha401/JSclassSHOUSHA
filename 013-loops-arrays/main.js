d2fb7725e18a005291c228a280ad16f3a1d36177
shousha401 <shousha401@hotmail.com>
2023-02-20 15:48:04 -0800

    writing the average MPG using for loop

--- 013-loops-arrays/main.js
+++ 013-loops-arrays/main.js
@@ -1,5 +1,6 @@
 // update the dom
-const myArr = []
+const myMPG = []
+const myTripCost = []
 
 const updateDom = (input) => {
     const divEl = document.querySelector(`#output`)
@@ -11,19 +12,37 @@ const updateDom = (input) => {
 
 const trackMPGCost = (miles, gallons, price = 3.79) => {
     const MPG = Math.round (miles / gallons * price)
-    const tripCoast = gallons * price
+    const tripCoast = Math.round (gallons * price)
     updateDom(`Milage per gallon ${MPG} cost ${tripCoast}`)
-    myArr.push(MPG,tripCoast)
+    myMPG.push(MPG)
+    myTripCost.push(tripCoast)
   }
 
-  const calculateAvg = () => {
-    const avgMPG = (myArr[0]+ myArr[2]) / 2
-    updateDom(` Average miles per gallon is ${avgMPG} `)
+  const calculateMPGAvg = () => {
+    //const avgMPG = (myArr[0]+ myArr[2]) / 2
+   // updateDom(` Average miles per gallon is ${avgMPG} `)
+   let totalMPG = 0
+    for (let i = 0; i < myMPG.length; i++){
+       totalMPG = totalMPG + myMPG[i]
+    }
+    //console.log(totalMPG)
+    let avgMPG = totalMPG/myMPG.length
+    //return
+    updateDom(`avg mps is ${avgMPG}`)
+
   }
 
 
   
   trackMPGCost(300, 10, 5.4)
-  trackMPGCost(300, 10, 5)
+  trackMPGCost(400, 6, 5)
+  trackMPGCost(200, 5, 4)
+  trackMPGCost(150, 9, 4)
+  trackMPGCost(270, 4, 3)
+  trackMPGCost(180, 4, 6)
+  trackMPGCost(432, 6, 4)
+  trackMPGCost(210, 4, 2)
+  trackMPGCost(121, 3, 5)
+  trackMPGCost(390, 11, 9)
   //updateDom(myArr)
-  calculateAvg()
+  calculateMPGAvg()
