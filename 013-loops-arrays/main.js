d3cd040832af976f5381fde22d4c1d47f91ce71f
shousha401 <shousha401@hotmail.com>
2023-02-20 16:19:37 -0800

    git flow my code for avreagecoast

--- 013-loops-arrays/main.js
+++ 013-loops-arrays/main.js
@@ -10,7 +10,7 @@ const updateDom = (input) => {
     
 }
 
-const trackMPGCost = (miles, gallons, price = 3.79) => {
+const trackMPGandCost = (miles, gallons, price = 3.79) => {
     const MPG = Math.round (miles / gallons * price)
     const tripCoast = Math.round (gallons * price)
     updateDom(`Milage per gallon ${MPG} cost ${tripCoast}`)
@@ -19,30 +19,38 @@ const trackMPGCost = (miles, gallons, price = 3.79) => {
   }
 
   const calculateMPGAvg = () => {
-    //const avgMPG = (myArr[0]+ myArr[2]) / 2
-   // updateDom(` Average miles per gallon is ${avgMPG} `)
    let totalMPG = 0
     for (let i = 0; i < myMPG.length; i++){
        totalMPG = totalMPG + myMPG[i]
     }
-    //console.log(totalMPG)
-    let avgMPG = totalMPG/myMPG.length
-    //return
+    let avgMPG = Math.round(totalMPG/myMPG.length)
     updateDom(`avg mps is ${avgMPG}`)
+  }
+  //////////////////////////////////////////////////////////
 
+  const calculateAvgCoast = () => {
+    let totalCoast = 0
+    for (let i=0; i < myTripCost.length;i++){
+        totalCoast = totalCoast + myTripCost[i]
+    }
+    let avgCoast = Math.round(totalCoast/myTripCost.length)
+    updateDom(`avvg cost is ${avgCoast}`)
+       
   }
 
 
+  // calculateAvgCoast
+  // git flow my code for avreagecoast
+  trackMPGandCost(300, 10, 5.4)
+  trackMPGandCost(400, 6, 5)
+  trackMPGandCost(200, 5, 4)
+  trackMPGandCost(150, 9, 4)
+  trackMPGandCost(270, 4, 3)
+  trackMPGandCost(180, 4, 6)
+  trackMPGandCost(432, 6, 4)
+  trackMPGandCost(210, 4, 2)
+  trackMPGandCost(121, 3, 5)
+  trackMPGandCost(390, 11, 9)
   
-  trackMPGCost(300, 10, 5.4)
-  trackMPGCost(400, 6, 5)
-  trackMPGCost(200, 5, 4)
-  trackMPGCost(150, 9, 4)
-  trackMPGCost(270, 4, 3)
-  trackMPGCost(180, 4, 6)
-  trackMPGCost(432, 6, 4)
-  trackMPGCost(210, 4, 2)
-  trackMPGCost(121, 3, 5)
-  trackMPGCost(390, 11, 9)
-  //updateDom(myArr)
   calculateMPGAvg()
+  calculateAvgCoast()
