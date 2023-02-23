07e652db76a4f03c3d4b108c3648372f1a1e07a5
shousha401 <shousha401@hotmail.com>
2023-02-22 20:45:20 -0800

    my loops

---
+++ 014-my-loops/main.js
@@ -0,0 +1,17 @@
+//let i = 0;
+
+//do {
+ // console.log(i);
+ // i++;
+//} while (i < 4);
+
+let i = 1;
+let sum = 0;
+
+do {
+  sum += i;
+  i++;
+} while (i <= 10);
+
+console.log("The sum of 1 to 10 is: " + sum);
+
