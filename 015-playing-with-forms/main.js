049cdc739eab73839d31e4653c06c55a6baf74fa
shousha401 <shousha401@hotmail.com>
2023-02-28 20:01:51 -0800

    my formm

---
+++ 015-playing-with-forms/main.js
@@ -0,0 +1,10 @@
+const form = document.getElementById("Form")
+const log = document.getElementById("log")
+
+form.addEventListener("submit", function(event) {
+  event.preventDefault()
+
+  form.reset()
+ 
+})
+
