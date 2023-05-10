497d525a116dd8b29505adf9b3db265018861df1
shousha401 <shousha401@hotmail.com>
2023-05-09 20:23:24 -0700

    fixing IO

---
+++ 025-DOM-selectors/main.js
@@ -0,0 +1,34 @@
+
+import {renderTable} from "./render.js";
+import { getTripData, saveTripData } from "./storage.js";
+import { isFormValid, calculateAvg, trackMPGandCost } from "./app.js"
+
+
+const FORM = document.getElementById('form-input');
+const ERR = document.getElementById('err');
+const AVG_OUTPUT = document.getElementById('output-avg');
+
+
+
+const MY_DATA = getTripData()
+renderTable(MY_DATA)
+
+
+
+FORM.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const miles = parseFloat(e.target.miles.value);
+    const gallons = parseFloat(e.target.gallons.value);
+    const price = parseFloat(e.target.price.value);
+    const isValid = isFormValid(miles, gallons, price);
+    if (isValid) {
+        ERR.textContent = '';
+        AVG_OUTPUT.textContent = '';
+        const dataObj = trackMPGandCost(miles, gallons, price);
+        MY_DATA.push(dataObj);
+        saveTripData()
+        renderTable(MY_DATA, FORM);
+        calculateAvg();
+    }
+    FORM.reset();
+});
