593a98b504ea0fba545899239a1e51d391bcd9cd
shousha401 <shousha401@hotmail.com>
2023-05-09 23:00:41 -0700

    student subclass

--- 027-intro-to-oop/main.js
+++ 027-intro-to-oop/main.js
@@ -8,11 +8,23 @@ class Person {
 
     }
     showStatus(){
-        console.log (`${this.fristName} lives at ${this.address}`)
+        console.log (`${this.fristName} lives at ${this.address} is taking ${this.classList}`)
     }
 
+    
+
 }
-const eslam = new Person("eslam", "shousha","12 home")
+
+class Student extends Person {
+    constructor(fristName, lastName, address, classList){
+    super (fristName, lastName, address)
+    this.classList = classList
+    }
+        
+}
+
+
+const eslam = new Student("eslam", "shousha","12 home", ["eng1a", "cirj12"])
 eslam.showStatus()
 const moe = new Person("moe", "shousha","15 home")
 moe.showStatus()
