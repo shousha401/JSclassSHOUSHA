d277dd52885ec8f5454f8aa11860c9455c52b065
shousha401 <shousha401@hotmail.com>
2023-05-09 19:21:34 -0700

    redo user management

--- 100-my-app/main.js
+++ 100-my-app/main.js
@@ -1,74 +1,32 @@
 
-let users = [];
-let userIdCounter = 0;
-let maxAge = 60;
+let name = "Alice";
+let age = 25;
+let hasDriverLicense = true;
 
-function addUser() {
-  let name = document.getElementById("name").value;
-  let age = document.getElementById("age").value;
-  let email = document.getElementById("email").value;
- 
+// Calculate whether the person is eligible to rent a car
+let canRentCar = age >= 25 && hasDriverLicense;
 
-  if (name && age && email) {
-    let user = {
-      id: ++userIdCounter,
-      name: name,
-      age: parseInt(age),
-      email: email
-    };
-    users.push(user);
-    updateTable();
-    document.getElementById("name").value = "";
-    document.getElementById("age").value = "";
-    document.getElementById("email").value = "";
-    updateTable();
-  } else {
-    alert("Please fill out all fields");
-  }
+// Compare the person's name with a specified string
+let isNameMatch = name === "Bob" || name === "Alice";
+
+// Check if the person is a teenager
+let isTeenager = age >= 13 && age <= 19;
+
+// Display the decision output based on the variables
+if (canRentCar) {
+  console.log(`${name} can rent a car.`);
+} else {
+  console.log(`${name} cannot rent a car.`);
 }
 
-function deleteUser(id) {
-  let index = users.findIndex(user => user.id === id);
-  if (index !== -1) {
-    users.splice(index, 1);
-    updateTable();
-  }
+if (isNameMatch) {
+  console.log(`The name ${name} matches.`);
+} else {
+  console.log(`The name ${name} does not match.`);
 }
 
-function updateTable() {
-  let table = document.getElementById("user-table");
-  table.innerHTML = "";
-
-  let thead = document.createElement("thead");
-  let tr = document.createElement("tr");
-  tr.innerHTML = `
-    <th>Name</th>
-    <th>Age</th>
-    <th>Email</th>
-    <th>Action</th>
-  `;
-  thead.appendChild(tr);
-  table.appendChild(thead);
-
-  let tbody = document.createElement("tbody");
-  users.forEach(user => {
-    if (user.age > maxAge || user.email.endsWith(".edu")) {
-      let tr = document.createElement("tr");
-      tr.innerHTML = `
-        <td>${user.name}</td>
-        <td>${user.age}</td>
-        <td>${user.email}</td>
-        <td><button type="button" onclick="deleteUser(${user.id})">Delete</button></td>
-      `;
-      tbody.appendChild(tr);
-    }
-  });
-  table.appendChild(tbody);
+if (isTeenager) {
+  console.log(`${name} is a teenager.`);
+} else {
+  console.log(`${name} is not a teenager.`);
 }
-
-users.push({
-  id: ++userIdCounter,
-  name: "John Doe",
-  age: 25,
-  email: "johndoe@example.com"
-});
