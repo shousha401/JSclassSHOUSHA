2a9a7c80785f50a99d147ab2edf1c77b75fdab1b
shousha401 <shousha401@hotmail.com>
2023-05-11 19:10:50 -0700

    my attempt at oop coding challenge

--- 027-intro-to-oop/main.js
+++ 027-intro-to-oop/main.js
@@ -1,34 +1,64 @@
 class Person {
-    constructor(fristName, lastName, address){
-        this.fristName = fristName;
-        this.lastName = lastName;
-        this.address = address
-
-
-
+    constructor(firstName, lastName, address){
+        this._firstName = firstName;
+        this._lastName = lastName;
+        this._address = address;
     }
-    showStatus(){
-        console.log (`${this.fristName} lives at ${this.address}`)
+
+    getFirstName() {
+        return `${this._firstName}`
     }
+
+    setFirstName(newFirstName) {
+        this._firstName = newFirstName
+    }
+
+    getLastName() {
+        return `${this._lastName}`
+    }
+
+    setLastName(newLastName) {
+        this._lastName = newLastName
+    }
+
+    getAddress() {
+        return `${this._address}`
+    }
+    setAddress(newAddress) {
+        this.__address = newAddress
+    }
+
 
     
-
+    showStatus() {
+        console.log(`${this.getFirstName()} lives at ${this.getAddress()}`)
+    }
 }
 
 class Student extends Person {
-    constructor(fristName, lastName, address, classList){
-    super (fristName, lastName, address)
-    this.classList = classList
-    }
-    showStatus(){
-        console.log (`${this.fristName} lives at ${this.address} is taking ${this.classList}`)
+    constructor(firstName, lastName, address, classList) {
+        super(firstName, lastName, address)
+        this._classList = classList
     }
 
-        
+
+    getClassList() {
+        return `${this._classList}`
+    }
+
+    setClassList(newClassList) {
+        this.__classlist = newClassList
+    }
+
+    showStatus() {
+        console.log(`${this.getFirstName()} lives at ${this.getAddress()} is taking ${this.getClassList()}`)
+    }
 }
 
-
-const eslam = new Student("eslam", "shousha","12 home", ["eng1a", "cirj12"])
+const eslam = new Student("eslam", "shousha", "5151 pluto ave", ["Math51", "Art89"])
 eslam.showStatus()
-const moe = new Person("moe", "shousha","15home")
+const moe = new Person("moe", "shoush", "inside the pyramids")
 moe.showStatus()
+console.log(eslam.getFirstName())
+console.log(moe.getAddress())
+console.log(eslam.getLastName())
