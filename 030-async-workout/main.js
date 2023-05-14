2e79aa831a13bab5b1ec7f231bbfc9a9ec03ec02
shousha401 <shousha401@hotmail.com>
2023-05-13 19:47:32 -0700

    my code

--- 030-async-workout/main.js
+++ 030-async-workout/main.js
@@ -1,10 +1,26 @@
-const formEl = document.getElementById('form')
-const output = document.getElementById('output')
+const formEl = document.getElementById('form');
+const output = document.getElementById('output');
 
-formEl.addEventListener('submit', function(e){
-    e.preventDefault();
-    const type = e.target.type.value
-    const reps = parseFloat(e.target.type.value)
-    const time = parseFloat(e.target.type.value)
-    formEl.reset()
-})
+formEl.addEventListener('submit', function(e) {
+  e.preventDefault();
+
+  const type = e.target.elements.type.value;
+  const reps = parseFloat(e.target.elements.reps.value);
+  const time = parseFloat(e.target.elements.time.value);
+
+  formEl.reset();
+
+  displayExerciseInfo(type, reps, function() {
+    performAsyncUpdate(time);
+  });
+});
+
+function displayExerciseInfo(type, reps, callback) {
+  
+  output.textContent = `Exercise: ${type}, Reps: ${reps}`;
+
+  
+  callback();
+}
+
+
