514efe7f2883377fcbbaad72896d7313c4e529d4
shousha401 <shousha401@hotmail.com>
2023-05-14 16:35:35 -0700

    workout refactor with promises

--- 030-async-workout/main.js
+++ 030-async-workout/main.js
@@ -1,27 +1,36 @@
 const formEl = document.getElementById('form')
 const output = document.getElementById('output')
+
 function updateDom(message, el) {
-    const newEl = document.createElement(el)
-    newEl.textContent = message
-    output.appendChild(newEl)
-
+  const newEl = document.createElement(el)
+  newEl.textContent = message
+  output.appendChild(newEl)
 }
+
 function startWorkout(type, reps, time, fn) {
-     fn(`start ${type} <> goal reps is ${reps}`, 'p')  
-    setTimeout(()=>{
-        fn(`stop${type}`, 'h1')
+  return new Promise((resolve, reject) => {
+    fn(`start ${type} <> goal reps is ${reps}`, 'p')
 
-       }, time * 1000)
-
-
+    setTimeout(() => {
+      fn(`stop ${type}`, 'h1')
+      resolve(); 
+    }, time * 1000)
+  });
 }
 
-formEl.addEventListener('submit', function(e){
-    e.preventDefault();
-    const type = e.target.type.value
-    const reps = parseFloat(e.target.reps.value)
-    const time = parseFloat(e.target.time.value)
-    startWorkout(type, reps, time,updateDom)
-    formEl.reset()
-})
+formEl.addEventListener('submit', function(e) {
+  e.preventDefault();
+  const type = e.target.type.value
+  const reps = parseFloat(e.target.reps.value)
+  const time = parseFloat(e.target.time.value)
+
+  startWorkout(type, reps, time, updateDom)
+    .then(() => {
+      formEl.reset(); 
+    })
+    .catch((error) => {
+      console.error(error); 
+    });
+});
+
 
