95ae107448c37e899c48a887f3f663175dd610c0
shousha401 <shousha401@hotmail.com>
2023-05-14 09:17:50 -0700

    more promise methods

--- 031-promises/main.js
+++ 031-promises/main.js
@@ -1,47 +1,42 @@
-function getWeather(){
-    return new Promise (function(resolve, reject){
-        setTimeout(()=> {
-            resolve('Cloudy')
+function fun1(){
+    return new Promise((resolve, reject)=>{
+        setTimeout(()=>{
+            resolve('Bad data')
 
-        }, 100)   
+        },100)
 
     })
+       
 }
 
-function getWeatherIcon(weather) {
-   return new Promise (function(resolve, reject){
-        setTimeout(()=> {
-            switch(weather){
-                case 'Sunny':
-                    resolve('🌞')
-                    break
-                case 'Cloudy':
-                        resolve('☁️')
-                    break
-                case 'Rainy' :
-                            resolve('☔')
-                     break
-                            default:
-                                reject('NO ICON FOUND')
+function fun2(){
+ 
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve('😒')
 
-            }
-        }, 100)   
+
+
+        },100)
 
     })
-
+       
 }
 
 function onSuccess(data){
-    console.log(`Success ${data}`)
+    console.log(`Success: ${data}`)
 }
 
-function onError(error){
-    console.log(`Error: ${error}`)
+function onError(errorCode){
+    console.log(`Error: ${errorCode}`)
 }
 
+function onFinally(){
+    console.log('FINALLY WE BE DONE YO')
+}
 
-
-
-getWeather()
-.then(getWeatherIcon)
-.then(onSuccess, onError)
+fun1()
+.then(fun2)
+.then(onSuccess)
+.catch(onError)
+.finally(onFinally)
