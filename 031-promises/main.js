4014d050a313e17d3b0e0171ca2085f2892416f1
shousha401 <shousha401@hotmail.com>
2023-05-14 17:02:00 -0700

    getting weather in Fresno.

--- 031-promises/main.js
+++ 031-promises/main.js
@@ -1,42 +1,61 @@
-function fun1(){
-    return new Promise((resolve, reject)=>{
-        setTimeout(()=>{
-            resolve('Bad data')
+//function fun1(){
+    //return new Promise((resolve, reject)=>{
+       // setTimeout(()=>{
+        //    resolve('Bad data')
 
-        },100)
+       // },100)
 
-    })
+    //})
        
-}
+//}
 
-function fun2(){
+//function fun2(){
  
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            resolve('😒')
+   // return new Promise((resolve, reject) => {
+     //   setTimeout(() => {
+       //     resolve('😒')
 
 
 
-        },100)
+      //  },100)
 
-    })
+  //  })
        
+//}
+
+//function onSuccess(data){
+   // console.log(`Success: ${data}`)
+//}
+
+//function onError(errorCode){
+   // console.log(`Error: ${errorCode}`)
+//}
+
+//function onFinally(){
+    //console.log('FINALLY WE BE DONE YO')
+//}
+
+//fun1()
+//.then(fun2)
+//.then(onSuccess)
+//.catch(onError)
+//.finally(onFinally)
+
+function fetchData(){
+
+    return new Promise(function(resolve, reject){
+fetch('https://api.weather.gov/gridpoints/HNX/53,100/forecast')
+.then(response => response.json())
+.then(data => resolve(data.properties.periods[1].temperture))
+    })
+}
+function displayData(weather){
+    console.log(weather)
 }
 
-function onSuccess(data){
-    console.log(`Success: ${data}`)
+function onError(err){
+    console.log(`Error:${err}`)
 }
 
-function onError(errorCode){
-    console.log(`Error: ${errorCode}`)
-}
+fetchData().then(displayData).catch(onError)
 
-function onFinally(){
-    console.log('FINALLY WE BE DONE YO')
-}
-
-fun1()
-.then(fun2)
-.then(onSuccess)
-.catch(onError)
-.finally(onFinally)
