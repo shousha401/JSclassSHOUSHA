1efb8f820ca52a9e72dbf017eb581666957c7cbb
shousha401 <shousha401@hotmail.com>
2023-05-15 21:04:13 -0700

    handle edit and delete

--- 100-my-app/main.js
+++ 100-my-app/main.js
@@ -26,7 +26,8 @@ function updateDOM(userData) {
   let outputElement = document.getElementById("output");
   outputElement.innerHTML = "";
 
-  for (let user of userData) {
+  for (let i = 0; i < userData.length; i++) {
+    let user = userData[i];
     let userDiv = document.createElement("div");
     userDiv.classList.add("user");
 
@@ -44,36 +45,80 @@ function updateDOM(userData) {
     }`;
     userDiv.appendChild(licenseElement);
 
+    let decisionElement = document.createElement("p");
+    decisionElement.textContent = `Decision: ${user.decisionOutput}`;
+    userDiv.appendChild(decisionElement);
+
+    let editButton = document.createElement("button");
+    editButton.textContent = "Edit";
+    editButton.addEventListener("click", function () {
+      editUser(i);
+    });
+    userDiv.appendChild(editButton);
+
+    let deleteButton = document.createElement("button");
+    deleteButton.textContent = "Delete";
+    deleteButton.addEventListener("click", function () {
+      deleteUser(i);
+    });
+    userDiv.appendChild(deleteButton);
+
     outputElement.appendChild(userDiv);
   }
 }
 
 
 
-document.getElementById("userForm").addEventListener("submit", function(event) {
+let userData = [];
+
+function editUser(index) {
+  let user = userData[index];
+  document.getElementById("name").value = user.name;
+  document.getElementById("age").value = user.age;
+  document.getElementById("hasDriverLicense").checked = user.hasDriverLicense;
+  userData.splice(index, 1);
+  updateDOM(userData);
+}
+
+function deleteUser(index) {
+  userData.splice(index, 1);
+  updateDOM(userData);
+}
+
+document.getElementById("userForm").addEventListener("submit", function (event) {
   event.preventDefault();
 
-  
   let name = document.getElementById("name").value;
   let age = document.getElementById("age").value;
   let hasDriverLicense = document.getElementById("hasDriverLicense").checked;
 
- 
   if (!name || !age) {
     alert("Please fill in all required fields.");
     return;
   }
 
- 
   age = parseInt(age);
 
-  
-  let userData = processUser(name, age, hasDriverLicense);
-  
+  let editIndex = -1;
+  for (let i = 0; i < userData.length; i++) {
+    if (userData[i].name === name) {
+      editIndex = i;
+      break;
+    }
+  }
+
+  if (editIndex !== -1) {
+    userData[editIndex] = processUser(name, age, hasDriverLicense)[0];
+  } else {
+    let newUser = processUser(name, age, hasDriverLicense)[0];
+    userData.push(newUser);
+  }
+
   updateDOM(userData);
 
   document.getElementById("name").value = "";
   document.getElementById("age").value = "";
   document.getElementById("hasDriverLicense").checked = false;
 });
+
 
